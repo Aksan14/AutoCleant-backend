80801b1cd383c688b1565c9d6adfc9fcc1bb7c1e
Aksan14 <105841107223@student.unismuh.ac.id>
2025-08-19 20:41:28 +0800

    fix cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,11 +2,23 @@ package middleware
 
 import (
 	"net/http"
+	"os"
+	"strings"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://front-end-reset-bph-zdkg.vercel.app")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+
+		// Check if the request origin is in the allowed list
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, ngrok-skip-browser-warning")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
